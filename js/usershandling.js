87b9e4f03631e94c25994433c2a55d88e3c3f080
Storm <esteban.palaciov@hotmail.com>
2020-09-17 20:24:40 -0500

    Registro e inicio de sesión

--- js/usershandling.js
+++ js/usershandling.js
@@ -1,12 +1,33 @@
-function registrar(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+function registrar() {
+  var email = document.getElementById("emailSignUp").value;
+  var password = document.getElementById("passwordSignUp").value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode, " ", errorMessage);
-      });
+  
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode, " ", errorMessage);
+    
+  });
+
+}
+
+function signIn() {
+  var email = document.getElementById("emailSignIn").value;
+  var password = document.getElementById("passwordSignIn").value;
+
+  
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    console.log(errorCode, " ", errorMessage);
+    
+  });
+  
+    console.log("Se autenticó correctamente");
+  
 }
