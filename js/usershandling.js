4b9cdcbfc4a3ed55586ea7985dedeaac3006a609
id4n <60779600+id4n@users.noreply.github.com>
2020-09-23 22:53:12 -0500

    Arreglos minimos de espacios vacios en archivos

--- js/usershandling.js
+++ js/usershandling.js
@@ -2,14 +2,12 @@ function registrar() {
   var email = document.getElementById("emailSignUp").value;
   var password = document.getElementById("passwordSignUp").value;
 
-  
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
     console.log(errorCode, " ", errorMessage);
-    
   });
 
 }
@@ -18,16 +16,12 @@ function signIn() {
   var email = document.getElementById("emailSignIn").value;
   var password = document.getElementById("passwordSignIn").value;
 
-  
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
 
     console.log(errorCode, " ", errorMessage);
-    
   });
-  
     console.log("Se autenticó correctamente");
-  
 }
