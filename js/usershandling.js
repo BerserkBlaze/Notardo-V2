a9e26ef304e27e137209eaa64e4fc69fd72261ca
Storm <esteban.palaciov@hotmail.com>
2020-09-23 23:12:21 -0500

    Cambios login

--- js/usershandling.js
+++ js/usershandling.js
@@ -1,33 +1,35 @@
-function registrar() {
+/* Creación de usuarios */
+function signUp() {
   var email = document.getElementById("emailSignUp").value;
   var password = document.getElementById("passwordSignUp").value;
 
-  
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(function (){
+    console.log("Se creó la cuente correctamente");
+  })
+  .catch(function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ...
     console.log(errorCode, " ", errorMessage);
-    
   });
 
 }
 
+/* Inicio de sesión de los usuarios */
 function signIn() {
   var email = document.getElementById("emailSignIn").value;
   var password = document.getElementById("passwordSignIn").value;
 
-  
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function () {
+      console.log("Se autenticó correctamente");
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, " ", errorMessage);
+    });
 
-    console.log(errorCode, " ", errorMessage);
-    
-  });
-  
-    console.log("Se autenticó correctamente");
-  
+ 
+
 }
